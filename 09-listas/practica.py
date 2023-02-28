87c78171d15551dd4fc0f7587ac82e23a76a77fe
blood321 <andresguzman321@gmail.com>
2023-02-27 23:34:51 -0500

    algunas practicas

--- 09-listas/practica.py
+++ 09-listas/practica.py
@@ -1,4 +1,16 @@
-milista = ["Filosofía", "Matemática", "Física", "Química", "Historia", "Español"]
+# milista = ["Filosofía", "Matemática", "Física", "Química", "Historia", "Español"]
 
-for i in range(len(milista)):
-    print(f"Yo estudio {milista[i]}")
+# for i in range(len(milista)):
+#     print(f"Yo estudio {milista[i]}")
+
+
+# print("Ejemplo Lista números")
+
+# listaNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# listaNum.reverse()
+
+# print(listaNum)
+
+listaABC = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+

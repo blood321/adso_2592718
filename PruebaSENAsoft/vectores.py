b8d1647a3ea051993b998d930a768681c2ba0135
blood321 <andresguzman321@gmail.com>
2023-03-03 11:49:32 -0500

    ejercicio senasoft

---
+++ PruebaSENAsoft/vectores.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+pares = []
+impares = []
+
+n = int(input("ingresa el valor para la longitud de la lista: "))
+
+if n <= 20:
+    
+
+pares.append(input("ingresa un numero entero: "))
+
+# par = np.arange(2,20,2)
+
+# print(par)
